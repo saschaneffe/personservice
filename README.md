3344139b86b323c420d40c051e9431c645d1a692
Sascha Neffe <sascha.neffe@googlemail.com>
2020-11-19 09:20:38 +0100

    * added curl requests to README.md * service and webfronted should be usable

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# personservice
+# Requests (tested on Windows)
+
+### Get all persons
+
+curl -X GET http://localhost:8080/person
+
+### Get person by id
+
+curl -X GET http://localhost:8080/person/1
+
+### Add a new person
+
+curl -X POST -H "Content-Type: application/json" -d "{ ""firstName"": ""Max"", ""lastName"": ""Mustermann"", ""birthday"": ""2002-02-28"", ""gender"": ""Male"", ""size"": 180, ""weight"": 80 }" http://localhost:8080/person
+
+### Change a person
+
+curl -X PUT -H "Content-Type: application/json" -d "{ ""firstName"": ""Erika"", ""lastName"": ""Mustermann"", ""birthday"": ""2003-06-15"", ""gender"": ""Female"", ""size"": 160, ""weight"": 60 }" http://localhost:8080/person/1
+
+### Delete person by id
+
+curl -X DELETE http://localhost:8080/person/1
--- target/classes/static/js/app.js
+++ target/classes/static/js/app.js
@@ -1,37 +1,136 @@
+Vue.filter('formatDate', function(value) {
+    if (value) {
+        return moment(String(value)).format('YYYY-MM-DD')
+    }
+});
+
 var app = new Vue({
     el: "#app",
     data: {
-        persons: [{
-            id: 1,
-            firstname: "Max"
-        }],
+        persons: [],
         editData: {
             id: null,
-            firstname: null,
+            firstName: null,
+            lastName: null,
+            gender: null,
+            birthday: null,
+            size: null,
+            weight: null,
             mode: 0
-        }
+        },
+        infoData: {
+          text: ""
+        },
+        genders: ["Male", "Female"]
     },
     methods: {
-        addPerson: function () {
-            this.updatePerson(1);
+        openAddPerson: function () {
+            this.openUpdatePerson(1);
         },
-        editPerson: function(personToEdit) {
-            this.updatePerson(2, personToEdit);
+        openEditPerson: function(personToEdit) {
+            this.openUpdatePerson(2, personToEdit);
         },
-        updatePerson: function (mode, personToEdit) {
+        openUpdatePerson: function (mode, personToEdit) {
             if(mode !== 0) {
+
                 if(mode === 2) {
-                    this.editData.id = personToEdit.id;
-                    this.editData.firstname = personToEdit.firstname;
-                    this.editData.mode = 2;
+                    this.editData = {
+                        id: personToEdit.id,
+                        firstName: personToEdit.firstName,
+                        lastName: personToEdit.lastName,
+                        gender: personToEdit.gender,
+                        birthday: personToEdit.birthday,
+                        size: personToEdit.size,
+                        weight: personToEdit.weight,
+                        mode: 2
+                    };
                 } else {
-                    this.editData.id = null;
-                    this.editData.firstname = null;
-                    this.editData.mode = 1;
+                    this.editData = {
+                        id: null,
+                        firstName: null,
+                        lastName: null,
+                        gender: null,
+                        birthday: null,
+                        size: null,
+                        weight: null,
+                        mode: 1
+                    };
+                }
+
+                if(this.editData.birthday !== null) {
+                    this.editData.birthday = moment(this.editData.birthday).format("YYYY-MM-DD");
                 }
 
                 $("#personEditModal").modal();
             }
+        },
+        updatePerson: function () {
+            let self = this;
+            $("#personEditModal").modal('hide');
+            let method = self.editData.mode === 1 ? 'POST' : 'PUT';
+            let url = "person" + (self.editData.mode === 1 ? "" : "/" + self.editData.id);
+            let data = JSON.stringify(self.editData);
+
+            $.ajax(url, {
+                method: method,
+                data: data,
+                contentType: 'application/json'
+            }).then(function() {
+                self.infoData.text = "The person was successfully edited";
+                self.infoData.title = "Edit successful";
+                self.refreshPersons();
+                $("#infoTextModal").modal();
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                self.infoData.text = "An error occured during edit of person.\n" + textStatus + " " + errorThrown;
+                self.infoData.title = "Edit failed";
+                $("#infoTextModal").modal();
+            });
+        },
+        refreshPersons: function () {
+            let self = this;
+            $.get({ url: 'person', dataType: 'json', success: function (content) {
+                self.persons = content.map(element => {
+                    if(element.birthday !== null) {
+                        element.birthday = moment(element.birthday).toDate();
+                    }
+
+                    return element;
+                });
+            }})
+        },
+        openDeletePerson: function (id) {
+            this.editData = {
+                id: id,
+                firstName: null,
+                lastName: null,
+                gender: null,
+                birthday: null,
+                size: null,
+                weight: null,
+                mode: 3
+            };
+            $("#personDeleteModal").modal();
+        },
+        deletePerson: function () {
+            $("#personDeleteModal").modal('hide');
+            let self = this;
+            if(self.editData.mode === 3) {
+                $.ajax({
+                    url: "person/" + self.editData.id,
+                    method: "DELETE"
+                }).then(function () {
+                    self.infoData.text = "The person was successfully deleted";
+                    self.infoData.title = "Deletion successful";
+                    self.refreshPersons();
+                    $("#infoTextModal").modal();
+                }).fail(function (jqXHR, textStatus, errorThrown) {
+                    self.infoData.text = "An error occured during deletion of person.\n" + errorThrown;
+                    self.infoData.title = "Deletion failed";
+                    $("#infoTextModal").modal();
+                })
+            }
         }
     }
-})
+});
+
+app.refreshPersons();
